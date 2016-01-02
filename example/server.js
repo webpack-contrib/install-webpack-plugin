7bcb1ae1d3dc4122e79608bda2ca1da465d44433
Eric Clemmons <eric@smarterspam.com>
2016-01-02 15:25:47 -0600

    Add server-side example

---
+++ example/server.js
@@ -0,0 +1,12 @@
+/**
+ * Uncomment things line-by-line & watch the loader go!
+ */
+
+// var express = require("express");
+//
+// express()
+//  .get("*", function(req, res) {
+//    res.send("Howdy!");
+//  })
+//  .listen(3000)
+//;
