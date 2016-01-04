6f8a96862416b7061c2d73eb7cf32862b9cfde80
Eric Clemmons <eric@smarterspam.com>
2016-01-04 13:14:32 -0600

    Improve server.js example

--- example/server.js
+++ example/server.js
@@ -2,11 +2,22 @@
  * Uncomment things line-by-line & watch the loader go!
  */
 
-// var express = require("express");
-//
-// express()
-//  .get("*", function(req, res) {
-//    res.send("Howdy!");
-//  })
-//  .listen(3000)
-//;
+var config = require("./webpack.config.client");
+var dev = require("webpack-dev-middleware");
+var express = require("express");
+var hot = require("webpack-hot-middleware");
+var path = require("path");
+var webpack = require("webpack");
+
+var compiler = webpack(config);
+
+express()
+  .use(dev(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath,
+    quiet: true,
+  }))
+  .use(hot(compiler, { reload: true }))
+  .use(express.static("public"))
+  .listen(3000)
+;
