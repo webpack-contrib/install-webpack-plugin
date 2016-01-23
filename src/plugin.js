ec28fb14cd0d992e8a62abc1a694f17272cb17a6
Eric Clemmons <eric@smarterspam.com>
2016-01-22 20:06:10 -0600

    Refactor plugin

--- src/plugin.js
+++ src/plugin.js
@@ -1,3 +1,5 @@
+var path = require("path");
+
 var installer = require("./installer");
 
 function NpmInstallPlugin(options) {
@@ -7,30 +9,26 @@ function NpmInstallPlugin(options) {
 NpmInstallPlugin.prototype.apply = function(compiler) {
   var cli = this.options.cli;
 
+  compiler.resolvers.normal.plugin("module", function(result, next) {
+    if (result.path.match("node_modules")) {
+      return next();
+    }
+
+    var dep = installer.check(result.request);
+
+    // Dependency needs to be installed
+    if (dep) {
+      installer.install(dep, cli);
+    }
+
+    next();
+  });
+
   compiler.plugin("normal-module-factory", function(factory) {
-    console.log("normal-module-factory");
-
-    factory.plugin("before-resolve", function(result, callback) {
-      var context = result.context;
-      var request = result.request;
-
-      factory.resolvers.normal.resolve(context, request, function(err, filepath) {
-        if (filepath) {
-          return callback(null, result);
-        }
-
-        var missing = installer.check([request], []);
-
-        // Found in `require` paths
-        if (!missing.length) {
-          return callback(null, result);
-        }
-
-        console.log("Missing dependency", result, missing);
-
-        installer.install(missing, cli);
-
-        return callback(null, result);
+    factory.plugin("before-resolve", function(result, next) {
+      // Trigger early-module resolution
+      factory.resolvers.normal.resolve(result.context, result.request, function(err, filepath) {
+        next(null, result);
       });
     });
   });
