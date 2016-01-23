f0cd8564bbbb9aa2b3ce568fbb62c35747296cc3
Eric Clemmons <eric@smarterspam.com>
2016-01-23 01:18:47 -0600

    babel-loader ignores node_modules

--- example/webpack.config.babel.js
+++ example/webpack.config.babel.js
@@ -13,7 +13,7 @@ export const defaults = {
       { test: /\.css$/, loader: "style-loader" },
       { test: /\.css$/, loader: "css-loader", query: { localIdentName: "[name]-[local]--[hash:base64:5]" } },
       { test: /\.eot$/, loader: "file-loader" },
-      { test: /\.js$/, loader: "babel-loader", query: { cacheDirectory: true } },
+      { test: /\.js$/, loader: "babel-loader", query: { cacheDirectory: true }, exclude: /node_modules/ },
       { test: /\.json$/, loader: "json-loader" },
       { test: /\.(png|jpg)$/, loader: "url-loader", query: { limit: 8192 } }, // Inline base64 URLs for <= 8K images
       { test: /\.svg$/, loader: "url-loader", query: { mimetype: "image/svg+xml" } },
