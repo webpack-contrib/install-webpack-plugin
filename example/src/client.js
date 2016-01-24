dec4b565c9286b27c20fe5664ea62bad5a962a5b
Eric Clemmons <eric@smarterspam.com>
2016-01-24 17:40:32 -0600

    Fix capitalization of import

--- example/src/client.js
+++ example/src/client.js
@@ -1,6 +1,6 @@
 import React from "react";
 import DOM from "react-dom";
 
-import App from "./components/app";
+import App from "./components/App";
 
 DOM.render(<App />, document.getElementById("app"));
