e1d0308d3b7a79b6577b32e71a2081d5ca34cc81
Eric Clemmons <eric@smarterspam.com>
2016-01-23 23:40:02 -0600

    plugin + test

--- src/plugin.js
+++ src/plugin.js
@@ -6,38 +6,49 @@ function NpmInstallPlugin(options) {
   this.options = options || {};
 }
 
-NpmInstallPlugin.prototype.installModule = function(result, next) {
+NpmInstallPlugin.prototype.apply = function(compiler) {
+  // Plugin needs to intercept module resolution before the "official" resolve
+  compiler.plugin("normal-module-factory", this.listenToFactory);
+
+  // Install loaders on demand
+  compiler.resolvers.loader.plugin("module", this.resolveLoader.bind(this));
+
+  // Install project dependencies on demand
+  compiler.resolvers.normal.plugin("module", this.resolveModule.bind(this));
+};
+
+NpmInstallPlugin.prototype.listenToFactory = function(factory) {
+  factory.plugin("before-resolve", function(result, next) {
+    // Trigger early-module resolution
+    factory.resolvers.normal.resolve(result.context, result.request, function(err, filepath) {
+      next(null, result);
+    });
+  });
+};
+
+NpmInstallPlugin.prototype.resolve = function(result) {
   var dep = installer.check(result.request);
 
   if (dep) {
     installer.install(dep, this.options.cli);
   }
 
+  return dep;
+};
+
+NpmInstallPlugin.prototype.resolveModule = function(result, next) {
+  // Only install direct dependencies, not sub-dependencies
+  if (!result.path.match("node_modules")) {
+    this.resolve(result);
+  }
+
   next();
 };
 
-NpmInstallPlugin.prototype.apply = function(compiler) {
-  // Install loaders on demand
-  compiler.resolvers.loader.plugin("module", this.installModule.bind(this));
+NpmInstallPlugin.prototype.resolveLoader = function(result, next) {
+  this.resolve(result);
 
-  // Install project dependencies on demand
-  compiler.resolvers.normal.plugin("module", function(result, next) {
-    // Skip dependencies of dependencies
-    if (result.path.match("node_modules")) {
-      return next();
-    }
-
-    this.installModule(result, next);
-  }.bind(this));
-
-  compiler.plugin("normal-module-factory", function(factory) {
-    factory.plugin("before-resolve", function(result, next) {
-      // Trigger early-module resolution
-      factory.resolvers.normal.resolve(result.context, result.request, function(err, filepath) {
-        next(null, result);
-      });
-    });
-  });
+  next();
 };
 
 module.exports = NpmInstallPlugin;
---
+++ test/plugin.test.js
@@ -0,0 +1,208 @@
+var expect = require("expect");
+var installer = require("../src/installer");
+var Plugin = require("../src/plugin");
+
+describe("plugin", function() {
+  beforeEach(function() {
+    this.options = {
+      cli: {
+        save: true,
+        saveDev: false,
+      },
+    };
+
+    this.plugin = new Plugin(this.options);
+  });
+
+  it("should accept options", function() {
+    expect(this.plugin.options).toEqual(this.options);
+  });
+
+  describe(".apply", function() {
+    before(function() {
+      this.compiler = {
+        plugin: expect.createSpy(),
+        resolvers: {
+          loader: { plugin: expect.createSpy() },
+          normal: { plugin: expect.createSpy() },
+        },
+      };
+
+      this.plugin.apply(this.compiler);
+    });
+
+    after(function() {
+      expect.restoreSpies();
+    });
+
+    it("should hook into `normal-module-factory`", function() {
+      expect(this.compiler.plugin.calls.length).toBe(1);
+      expect(this.compiler.plugin.calls[0].arguments).toEqual([
+        "normal-module-factory",
+        this.plugin.listenToFactory
+      ]);
+    });
+
+    it("should hook into loader resolvers", function() {
+      expect(this.compiler.resolvers.loader.plugin.calls.length).toBe(1);
+      expect(this.compiler.resolvers.loader.plugin.calls[0].arguments).toEqual([
+        "module",
+        this.plugin.resolveLoader.bind(this.plugin)
+      ]);
+    });
+
+    it("should hook into normal resolvers", function() {
+      expect(this.compiler.resolvers.normal.plugin.calls.length).toBe(1);
+      expect(this.compiler.resolvers.normal.plugin.calls[0].arguments).toEqual([
+        "module",
+        this.plugin.resolveModule.bind(this.plugin)
+      ]);
+    });
+  });
+
+  describe(".listenToFactory", function() {
+    before(function() {
+      this.next = expect.createSpy().andCall(function(err, result) {
+        return result;
+      });
+
+      this.result = {
+        context: "/",
+        request: "foo",
+        path: "node_modules"
+      };
+
+      this.factory = {
+        plugin: expect.createSpy().andCall(function(event, factoryPlugin) {
+          factoryPlugin(this.result, this.next);
+        }.bind(this)),
+
+        resolvers: {
+          normal: {
+            resolve: expect.createSpy().andCall(function(context, request, callback) {
+              callback(null, [context, request].join("/"));
+            }.bind(this)),
+          },
+        },
+      };
+
+      this.plugin.listenToFactory(this.factory);
+    });
+
+    it("should hook into `before-resolve`", function() {
+      expect(this.factory.plugin.calls.length).toBe(1);
+      expect(this.factory.plugin.calls[0].arguments[0]).toBe("before-resolve");
+    });
+
+    it("should immediately resolve", function() {
+      expect(this.factory.resolvers.normal.resolve.calls.length).toBe(1);
+    });
+
+    it("should pass result through", function() {
+      expect(this.next.calls.length).toBe(1);
+      expect(this.next.calls[0].arguments).toEqual([null, this.result]);
+    });
+  });
+
+  describe(".resolve", function() {
+    beforeEach(function() {
+      this.check = expect.spyOn(installer, "check");
+      this.install = expect.spyOn(installer, "install");
+      this.result = { request: "foo "};
+    });
+
+    afterEach(function() {
+      this.check.restore();
+      this.install.restore();
+    });
+
+    it("should check if request is installed", function() {
+      this.plugin.resolve(this.result);
+
+      expect(this.check.calls.length).toBe(1);
+      expect(this.check.calls[0].arguments).toEqual([this.result.request]);
+    });
+
+    it("should return the value of the check", function() {
+      this.check.andReturn(this.result.request);
+
+      var result = this.plugin.resolve(this.result);
+
+      expect(this.check.calls.length).toBe(1);
+      expect(this.check.calls[0].arguments).toEqual([this.result.request]);
+
+      expect(result).toEqual(this.result.request);
+    });
+
+    it("should not install if it exists", function() {
+      this.plugin.resolve(this.result);
+
+      expect(this.install.calls.length).toEqual(0);
+    });
+
+    it("should install if missing", function() {
+      this.check.andReturn(this.result.request);
+      this.plugin.resolve(this.result);
+
+      expect(this.install.calls.length).toBe(1);
+      expect(this.install.calls[0].arguments).toEqual([
+        this.result.request,
+        this.options.cli
+      ]);
+    });
+  });
+
+  describe(".resolveModule", function() {
+    beforeEach(function() {
+      this.resolve = expect.spyOn(this.plugin, "resolve");
+      this.next = expect.createSpy();
+    });
+
+    afterEach(function() {
+      this.resolve.restore();
+      this.next.restore();
+    });
+
+    it("should call .resolve if direct dependency", function() {
+      var result = { path: "/", request: "foo" };
+
+      this.plugin.resolveModule(result, this.next);
+
+      expect(this.resolve.calls.length).toBe(1);
+      expect(this.resolve.calls[0].arguments).toEqual([result]);
+      expect(this.next.calls.length).toBe(1);
+      expect(this.next.calls[0].arguments).toEqual([]);
+    });
+
+    it("should call not .resolve if sub-dependency", function() {
+      var result = { path: "node_modules", request: "foo" };
+
+      this.plugin.resolveModule(result, this.next);
+
+      expect(this.resolve.calls.length).toBe(0);
+      expect(this.next.calls.length).toBe(1);
+      expect(this.next.calls[0].arguments).toEqual([]);
+    });
+  });
+
+  describe(".resolveLoader", function() {
+    beforeEach(function() {
+      this.resolve = expect.spyOn(this.plugin, "resolve");
+      this.next = expect.createSpy();
+    });
+
+    afterEach(function() {
+      this.resolve.restore();
+      this.next.restore();
+    });
+
+    it("should call .resolve", function() {
+      var result = { path: "node_modules", request: "foo" };
+
+      this.plugin.resolveLoader(result, this.next);
+
+      expect(this.resolve.calls.length).toBe(1);
+      expect(this.resolve.calls[0].arguments).toEqual([result]);
+    });
+  });
+});
