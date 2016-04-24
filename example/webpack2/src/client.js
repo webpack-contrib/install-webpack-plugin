2e172b87ecc6c921a5a4a58f87aaffddb927b57d
Eric Clemmons <eric@smarterspam.com>
2016-04-24 12:34:53 -0500

    Add webpack2 example

---
+++ example/webpack2/src/client.js
@@ -0,0 +1,7 @@
+// Found in /lib via webpack's resolve.root
+import App from "App";
+
+// Testing inline-loaders
+import loadRender from "bundle?lazy!./render";
+
+loadRender(({ render }) => render(App));
