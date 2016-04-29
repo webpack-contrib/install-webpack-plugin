c5b2c43bc8b868ffe5f1237d805fd4fb5c4fb859
Eric Clemmons <eric@smarterspam.com>
2016-04-29 12:40:30 -0500

    Create webpack1-dev-server example

---
+++ example/webpack1-dev-server/webpack.config.defaults.js
@@ -0,0 +1,42 @@
+var NpmInstallPlugin = require("npm-install-webpack-plugin");
+var path = require("path");
+
+module.exports = {
+  context: process.cwd(),
+
+  externals: [],
+
+  module: {
+    loaders: [
+      { test: /\.css$/, loader: "style" },
+      { test: /\.css$/, loader: "css", query: { localIdentName: "[name]-[local]--[hash:base64:5]" } },
+      { test: /\.eot$/, loader: "file" },
+      { test: /\.js$/, loader: "babel", query: { cacheDirectory: true }, exclude: /node_modules/ },
+      { test: /\.json$/, loader: "json" },
+      { test: /\.(png|jpg)$/, loader: "url", query: { limit: 8192 } }, // Inline base64 URLs for <= 8K images
+      { test: /\.svg$/, loader: "url", query: { mimetype: "image/svg+xml" } },
+      { test: /\.ttf$/, loader: "url", query: { mimetype: "application/octet-stream" } },
+      { test: /\.(woff|woff2)$/, loader: "url", query: { mimetype: "application/font-woff" } },
+    ],
+  },
+
+  output: {
+    chunkFilename: "[id].[hash:5]-[chunkhash:7].js",
+    devtoolModuleFilenameTemplate: "[absolute-resource-path]",
+    filename: "[name].js",
+  },
+
+  plugins: [
+    new NpmInstallPlugin(),
+  ],
+
+  resolve: {
+    alias: {
+      "react": "react-lite",
+      "react-dom": "react-lite",
+    },
+    root: [
+      path.join(process.cwd(), "lib"),
+    ],
+  },
+};
