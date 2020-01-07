d4d369f57147efcc01ff9451abce66cb609a437e
anikethsaha <anik220798@gmail.com>
2020-01-07 23:31:31 +0530

    chore: memory-fs -> memfs

--- src/plugin.js
+++ src/plugin.js
@@ -1,4 +1,4 @@
-var MemoryFS = require("memory-fs");
+var { createFsFromVolume, Volume } = require('memfs')
 var webpack = require("webpack");
 
 var installer = require("./installer");
@@ -98,7 +98,7 @@ NpmInstallPlugin.prototype.preCompile = function(compilation, next) {
     );
 
     this.preCompiler = webpack(config);
-    this.preCompiler.outputFileSystem = new MemoryFS();
+    this.preCompiler.outputFileSystem = createFsFromVolume(new Volume());
   }
 
   this.preCompiler.run(next);
